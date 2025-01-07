232e2f70fd2f49db9f5cc1e2e26c7525bbdbc87e
ahsanxtech <ahsan@xtecglobal.com>
2025-01-07 23:46:51 +0500

    waqar changes

--- verification-process.php
+++ verification-process.php
@@ -166,24 +166,24 @@
       <h2>Your <span>Privacy</span> Matters</h2>
     </div>
     <div class="row justify-content-center">
-      <div class="col-md-8">
+      <div class="col-md-12">
         <div class="privacy_points">
-          <ul class="list-unstyled">
-            <li>
+          <ul class="list-unstyled row">
+            <li class="col-md-4">
               <svg viewBox="0 0 24 24">
                 <path d="M12 1L3 5v6c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V5l-9-4z"></path>
               </svg>
               <h4>Any Card Works</h4>
               <p>Use a business or personal card—it's up to you.</p>
             </li>
-            <li>
+            <li class="col-md-4">
               <svg viewBox="0 0 24 24">
                 <path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm3.1-9H8.9V6c0-1.71 1.39-3.1 3.1-3.1 1.71 0 3.1 1.39 3.1 3.1v2z"></path>
               </svg>
               <h4>Identity Not Stored</h4>
               <p>We don't store or display your personal or payment details.</p>
             </li>
-            <li>
+            <li class="col-md-4">
               <svg viewBox="0 0 24 24">
                 <path d="M12 1L3 5v6c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V5l-9-4zm0 10.99h7c-.53 4.12-3.28 7.79-7 8.94V12H5V6.3l7-3.11v8.8z"></path>
               </svg>
